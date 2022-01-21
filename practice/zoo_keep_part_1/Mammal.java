0b09865fad6c86ef31b8d4b1a2cdcc65636b0213
robfara95 <robfara@gmail.com>
2022-01-21 15:29:49 -0500

    practice Zoo Keeper Part 1

---
+++ practice/zoo_keep_part_1/Mammal.java
@@ -0,0 +1,12 @@
+package animals;
+
+public class Mammal {
+	protected int energyLevel;
+	public Mammal(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+	public int displayEnergy() {
+		System.out.println("Energy Level: " + this.energyLevel);
+		return this.energyLevel;
+	}
+}
